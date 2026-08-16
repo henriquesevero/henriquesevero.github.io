80c1920c4adb79237ba2285e3086c19317c57663
Henrique Severo <contato@henriquesevero.com>
2026-08-16 18:20:02 -0300

    feat: publish "O Modelo OSI: Compreendendo a Arquitetura das Redes de Computadores"

--- src/articles/data.js
+++ src/articles/data.js
@@ -5,6 +5,15 @@ window.ARTICLES_DATA = {
   },
   articles: [
     {
+      slug:     'o-modelo-osi-compreendendo-a-arquitetura-das-redes-de-computadores',
+      title:    'O Modelo OSI: Compreendendo a Arquitetura das Redes de Computadores',
+      excerpt:  'Como o seu computador consegue abrir um site que está do outro lado do mundo? Para explicar essa mágica, os cientistas criaram o Modelo OSI.',
+      date:     '2026-08-16',
+      readTime: 5,
+      tags:     ['Computer', 'Networks', 'Architecture'],
+      cover:    'covers/o-modelo-osi-compreendendo-a-arquitetura-das-redes-de-computadores.jpeg',
+    },
+    {
       slug:     'modelo-de-concorrencia-em-go-go-routines-channels-e-padroes-de-comunicacao',
       title:    'Modelo de Concorrência em Go: Go Routines, Channels e Padrões de Comunicação',
       excerpt:  'Go não apenas suporta concorrência, ela foi projetada em torno dela. Enquanto a maioria das linguagens trata concorrência como uma biblioteca ou extensão, Go a torna um cidadão de primeira classe da linguagem. Entender o modelo de concorrência de Go profundamente é a diferença entre escrever código que funciona e código que escala.',
